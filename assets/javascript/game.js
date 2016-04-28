41a32994b0f400899aeac0689d6d603e4ca42a2c
Suzanne Kirk <sgraykirk@gmail.com>
2016-04-28 15:51:39 -0400

    added javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,16 @@
 //On start: rand target number chosen btwn 19-120, target number push to html, rand cat #s chosen 1-12
 window.onload = function(event) {
-
+	this.complete = false;
 	target = Math.floor((Math.random() * 120) + 19);
 	cat1 = Math.floor((Math.random() * 12) + 1);
 	cat2 = Math.floor((Math.random() * 12) + 1);
 	cat3 = Math.floor((Math.random() * 12) + 1);
 	cat4 = Math.floor((Math.random() * 12) + 1);
-	current = 0;     
+	current = 0;
+    
 	$("#target").html(target);
+	$("#wins").html(wins);
+	$("#losses").html(losses);
 
 	console.log(cat1, cat2, cat3, cat4);
 
@@ -25,14 +28,42 @@ window.onload = function(event) {
 		current = current+cat3;	
 	});
 	$('#cat4').on('click', function() {
-		$('#current').html(current+cat4);
+		$('#current').html(current+cat2);
 		current = current+cat4;	
 	});
 
 
 
 
+//if target = current, alert player win, restart game
+//if target < current, alert player loss, restart game
+
+	wins = 0;
+	losses = 0; 
+
+	if (target === current) {
+
+		alert("Congratulations! You have won!");
+    	$('#wins').html(wins++);
+    	this.complete = true;
+    	
+
+  } if (target < current) {
+
+  		alert("You have lost! Please try again.");
+    	$('#losses').html(losses++);
+    	this.complete = true;
+    	
+    
+  }
+
+
 }
+
+
+
+
+
 
 
 
